699780172dbc30040c2192cd15aef8dc5948038d
Nava <axelnava3105@gmail.com>
2025-05-17 16:26:03 -0500

    yay_script

---
+++ scripts_Installation/yay_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# Clona el repositorio de yay
+git clone https://aur.archlinux.org/yay.git
+
+# Cambia al directorio de yay
+cd yay
+
+# Instala yay
+makepkg -si --noconfirm
+
+# Sale y borra el directorio
+cd ..
+sudo rm -rf yay
+
+# Muestra el mensaje de instalación
+sleep 3
+echo "yay ha sido instalado correctamente."
